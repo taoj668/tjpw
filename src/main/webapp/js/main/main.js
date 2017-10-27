37475845231920e65f0528fc2539555f158e8e01
taoj668 <taoj668@gmail.com>
2017-10-28 04:34:07 +0800

    主页大致框架搭建完成

---
+++ src/main/webapp/js/main/main.js
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+$(function(){
+        <!--调用Luara-->
+/*        $(".banner-left").luara({width:"240",height:"405",interval:4000,selected:"seleted",deriction:"top"});*/
+        $(".banner-right").luara({width:"720",height:"405",interval:3000,selected:"seleted"});
+
+    });
